bd71a6a5135820adf2c26c9bd54e4310e4ce2e1b
Addson <addsonvinicyus@gmail.com>
2025-04-28 15:31:07 -0300

    Lógica da geração de números adicionada

--- app/src/main/java/com/example/megasenacompose/MainActivity.kt
+++ app/src/main/java/com/example/megasenacompose/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.megasenacompose
 
 import android.R.attr.top
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -23,6 +24,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,11 +35,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.megasenacompose.ui.theme.GreenMain
 import com.example.megasenacompose.ui.theme.MegaSenaComposeTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,70 +50,119 @@ class MainActivity : ComponentActivity() {
         setContent {
             MegaSenaComposeTheme {
                 Surface(
-
                 ) {
                     MegaSenaApp()
                 }
             }
         }
     }
-}
 
-@Composable
-fun EditNumberField(modifier: Modifier = Modifier){
-    OutlinedTextField(
-        value = "",
-        onValueChange = {},
-        singleLine = true,
-        label = { Text(
-            stringResource(R.string.number_hint),
-            color = Color(0xFFFFFFFF)
-        ) },
-        keyboardOptions = KeyboardOptions.Default.copy(
-            keyboardType = KeyboardType.Number,
-            imeAction = ImeAction.Done
-        ),
-        modifier = modifier
-    )
-}
-
-
-@Preview
-@Composable
-fun MegaSenaApp(modifier: Modifier = Modifier){
-
-    Column(
-        modifier = modifier
-            .background(GreenMain)
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-
-    ){
-        Image(
-            painter = painterResource(R.drawable.mega_sena_logo),
-            contentDescription = stringResource(R.string.logo_desc),
-            modifier = modifier.padding(top = 70.dp)
-        )
-        Spacer(modifier = Modifier.height(50.dp))
-        EditNumberField()
-        Spacer(modifier = Modifier.height(30.dp))
-        Text(
-            text = "Resultado",
-            fontSize = 20.sp,
-            color = Color(0xFFFFFFFF)
-        )
-        Spacer(modifier = Modifier.height(40.dp))
-        Button(
-            onClick = {},
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFFFFF))
-        ){
-            Text(
-                stringResource(R.string.gen_numbers_txt),
-                color = GreenMain
-                )
-        }
+    private fun displayError(){
+        Toast.makeText(this, "Informe um número entre 6 e 15", Toast.LENGTH_LONG).show()
     }
 
+    private fun numberGenerator(numberInput: String): String{
+        if(numberInput.isEmpty()){
+            displayError()
+            return "Resultado"
+        }
+
+        val numberQtd = numberInput.toInt()
+
+        if(numberQtd < 6 || numberQtd > 15){
+            displayError()
+            return "Resultado"
+        }
+
+        val numbersList = mutableSetOf<Int>()
+        val random = java.util.Random()
+
+        while(numbersList.size < numberQtd){
+            val number = random.nextInt(60)
+            numbersList.add(number + 1)
+        }
+
+        val sortedList = numbersList.sorted()
+
+        return sortedList.joinToString(" - ")
+
+    }
+
+    @Composable
+    fun EditNumberField(
+        value: String,
+        onValueChange: (String) -> Unit,
+        modifier: Modifier = Modifier
+    ){
+        OutlinedTextField(
+            value = value,
+            onValueChange = onValueChange,
+            singleLine = true,
+            label = { Text(
+                stringResource(R.string.number_hint),
+                color = Color(0xFFFFFFFF)
+            ) },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            modifier = modifier
+        )
+    }
+
+    @Preview
+    @Composable
+    fun MegaSenaApp(modifier: Modifier = Modifier){
+
+        var numberInput by remember { mutableStateOf("") }
+        var result by remember { mutableStateOf("Resultado") }
+        //val number = numberInput.toIntOrNull() ?: 0
+
+        Column(
+            modifier = modifier
+                .background(GreenMain)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+
+            ){
+            Image(
+                painter = painterResource(R.drawable.mega_sena_logo),
+                contentDescription = stringResource(R.string.logo_desc),
+                modifier = modifier.padding(top = 70.dp)
+            )
+            Spacer(modifier = Modifier.height(50.dp))
+            EditNumberField(
+                value = numberInput,
+                onValueChange = { numberInput = it}
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+            Text(
+                text = result,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                color = Color(0xFFFFFFFF),
+                modifier = modifier.padding(start = 20.dp, end = 20.dp)
+            )
+            Spacer(modifier = Modifier.height(40.dp))
+            Button(
+                onClick = { result = numberGenerator(numberInput) },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFFFFFF))
+            ){
+                Text(
+                    stringResource(R.string.gen_numbers_txt),
+                    color = GreenMain
+                )
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
 
 
